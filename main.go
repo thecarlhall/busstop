fc5d97fea87823722877ea89a6a3ea21b90020ca
Carl Hall <carl@cloudability.com>
2016-02-27 10:59:21 -0800

    Cleanup message printing and code.  This is the last commit that uses page scraping.

--- main.go
+++ main.go
@@ -10,6 +10,33 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+func displayMessage(title string, message string, showGrowl bool) {
+	if showGrowl {
+		script := fmt.Sprintf("display notification \"%s\" with title \"%s\"", message, title)
+		cmd := exec.Command("/usr/bin/osascript", "-e", script)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println(title)
+		fmt.Println(message)
+	}
+}
+
+func titleAndMessage(doc *goquery.Document, locationId int) (string, string) {
+	title := fmt.Sprintf("Bus Stop - %d", locationId)
+
+	selection := doc.Find("ul#arrivalslist.group > li")
+	messages := make([]string, selection.Length())
+	selection.Each(func(i int, s *goquery.Selection) {
+		messages[i] = fmt.Sprint(strings.TrimSpace(s.Find("h3").Text()), "\n    ", s.Find("p.clear").Text(), " in ", s.Find("p.arrival").Text())
+	})
+
+	message := strings.Join(messages, "\n")
+
+	return title, message
+}
+
 func main() {
 	growl := flag.Bool("growl", false, "whether to use growl notifications")
 	locationId := flag.Int("locationId", 13168, "location ID to track")
@@ -20,23 +47,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	title := fmt.Sprintf("Bus Stop - %d", *locationId)
-
-	selection := doc.Find("ul#arrivalslist.group > li")
-	messages := make([]string, selection.Length())
-	selection.Each(func(i int, s *goquery.Selection) {
-		messages[i] = fmt.Sprint(strings.TrimSpace(s.Find("h3").Text()), "\n    ", s.Find("p.clear").Text(), " in ", s.Find("p.arrival").Text())
-	})
-
-	message := strings.Join(messages, "\n")
-	if *growl {
-		script := fmt.Sprintf("display notification \"%s\" with title \"%s\"", message, title)
-		cmd := exec.Command("/usr/bin/osascript", "-e", script)
-		if err = cmd.Run(); err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		fmt.Println(title)
-		fmt.Println(message)
-	}
+	title, message := titleAndMessage(doc, *locationId)
+	displayMessage(title, message, *growl)
 }
